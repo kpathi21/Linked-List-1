8320b4e55d05be743ac1a0c5b43d8ddd8207b852
kpathi21 <kavyapathi21@gmail.com>
2025-04-08 17:03:16 -0400

    Completed Linked-List-1

---
+++ java/ReverseLinkedList.java
@@ -0,0 +1,48 @@
+package java;
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
+
+//TC: O(n)
+//SC: O(1)
+
+
+//Approach 2 - recursion
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode res = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return res;
+    }
+}
+
+//TC: O(n), SC: O(n)
